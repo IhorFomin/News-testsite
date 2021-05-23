0500a3c9faa203fc2ef5666c942a97e5b98206dd
ihor_fomin <ihor.fomin.mypage@gmail.com>
2021-05-23 20:57:26 +0300

    Working with forms in Django (L2)

--- news/forms.py
+++ news/forms.py
@@ -3,7 +3,8 @@ from .models import Category
 
 
 class NewsForm(forms.Form):
-    title = forms.CharField(max_length=150)
-    content = forms.CharField()
-    is_published = forms.BooleanField()
-    category = forms.ModelChoiceField(queryset=Category.objects.all())
+    title = forms.CharField(max_length=150, label='Название', widget=forms.TextInput(attrs={"class": "form-control"}))
+    content = forms.CharField(label='Текст', required=False, widget=forms.Textarea(attrs={"class": "form-control", "rows": "5"}))
+    is_published = forms.BooleanField(label='Опубликовано?', initial=True)
+    category = forms.ModelChoiceField(queryset=Category.objects.all(), label='Категория', empty_label='Выберите категорию',
+                                      widget=forms.Select(attrs={"class": "form-control"}))
--- news/views.py
+++ news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 
 from .models import News, Category
@@ -29,7 +29,11 @@ def view_news(request, news_id):
 
 def add_news(request):
     if request.method == 'POST':
-        pass
+        form = NewsForm(request.POST)
+        if form.is_valid():
+            # print(form.cleaned_data)
+            news = News.objects.create(**form.cleaned_data)
+            return redirect(news)
     else:
         form = NewsForm()
     return render(request, 'news/add_news.html', {'form': form})
