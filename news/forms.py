56c5eca4fed46a7dbd8b120d54f919c7fa7000ed
ihor_fomin <ihor.fomin.mypage@gmail.com>
2021-05-24 19:56:57 +0300

    Working with custom validators in Django

--- news/forms.py
+++ news/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import Category, News
+import re
+from django.core.exceptions import ValidationError
 
 
 class NewsForm(forms.ModelForm):
@@ -20,3 +22,9 @@ class NewsForm(forms.ModelForm):
             'content': forms.Textarea(attrs={"class": "form-control", "rows": "5"}),
             'category': forms.Select(attrs={"class": "form-control"}),
         }
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if re.match(r'\d', title):
+            raise ValidationError('Название не должно начинаться с цифры')
+        return title
