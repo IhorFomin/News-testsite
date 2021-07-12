b248b7594fedf41d11e0759c4d3ab0080864bb29
ihor_fomin <ihor.fomin.mypage@gmail.com>
2021-07-12 22:22:55 +0300

    Implementation of pagination in Django (func, L1)

--- news/urls.py
+++ news/urls.py
@@ -3,6 +3,7 @@ from .views import *
 
 
 urlpatterns = [
+    path('test/', test, name='test'),
     # path('', index, name='home'),
     path('', HomeNews.as_view(), name='home'),
     # path('category/<int:category_id>/', get_category, name='category'),
--- news/views.py
+++ news/views.py
@@ -3,10 +3,22 @@ from django.http import HttpResponse
 from django.views.generic import ListView, DetailView, CreateView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
 
 from .models import News, Category
 from .forms import NewsForm
 from .utils import MyMixin
+
+
+def test(request):
+    """
+    Реализация пажинации с помощью функции
+    """
+    objects = ['john1', 'paul2', 'george3', 'ringo4', 'john5', 'paul6', 'george7']
+    paginator = Paginator(objects, 2)
+    page_num = request.GET.get('page', 1)
+    page_objects = paginator.get_page(page_num)
+    return render(request, 'news/test.html', {'page_obj': page_objects})
 
 
 class HomeNews(MyMixin, ListView):
